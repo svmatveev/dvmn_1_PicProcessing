5e2be914092377ea82958e7e0a18eab8b3591270
svmatveev <54777529+svmatveev@users.noreply.github.com>
2022-02-02 21:44:07 +0300

    1st attempt to fix problems from review

--- main.py
+++ main.py
@@ -1,54 +1,80 @@
-from PIL import Image
 import argparse
-import os
+from pathlib import Path
+
+from PIL import Image
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Image processing parameters')
     parser.add_argument('path', type=str, help='Path to image')
-    parser.add_argument('--resolution', type=int, default=80, help='Thumbnail resolution')
+    parser.add_argument('--resolution',
+                        type=int,
+                        default=80,
+                        help='Thumbnail resolution')
+    parser.add_argument('--precision',
+                        type=int,
+                        default=10,
+                        help='Thumbnail resolution 0 <= precision <= 100.\
+                              Restricted with that range if other values \
+                              specified')                    
     args = parser.parse_args()
     return args
 
-def print_attributes(image: Image):
-    width = image.width
-    height = image.height
-    format = image.mode
- 
-    print(f"Ширина — {width}\nВысота — {height}\nЦветовая модель — {format}");
 
-def image_crop(image: Image,
-                offset_width_l: int,
-                offset_height_l: int,
-                offset_width_r: int,
-                offset_height_r: int) -> Image:
-    image_croped = image.crop((offset_width_l,
-                                offset_height_l,
-                                (image.width - offset_width_r),
-                                (image.height - offset_height_r)))
-    return image_croped
-                
-def shift_image_layer(rgb_image: Image,
-                    offset: int) -> Image:
-    image_red_ch, image_green_ch, image_blue_ch = image_rgb.split()
-    image_croped_red = image_crop(image_red_ch, offset*2, 0, 0, 0)
-    image_croped_blue = image_crop(image_blue_ch, 0, 0, offset*2, 0)
-    image_croped_green = image_crop(image_green_ch, offset, 0, offset, 0)
-    img = Image.merge("RGB", (image_croped_red, image_croped_green, image_croped_blue))
+def crop_image(image: Image,
+               left_width_offset: int,
+               left_height_offset: int,
+               right_width_offset: int,
+               right_height_offset: int) -> Image:
+    cropped_image = image.crop((left_width_offset,
+                                left_height_offset,
+                                image.width - right_width_offset,
+                                image.height - right_height_offset))
+    return cropped_image
+
+
+def shift_image_layers(rgb_image: Image,
+                       offset: int) -> Image:
+    red_image_ch, green_image_ch, blue_image_ch = rgb_image.split()
+    red_croped_image = Image.blend(crop_image(red_image_ch, offset * 2, 0, 0, 0),
+                                   crop_image(red_image_ch, offset, 0, offset, 0),
+                                   .5)
+    blue_croped_image = Image.blend(crop_image(blue_image_ch, 0, 0, offset * 2, 0),
+                                    crop_image(blue_image_ch, offset, 0, offset, 0),
+                                    .5)
+    green_croped_image = crop_image(green_image_ch, offset, 0, offset, 0)
+    img = Image.merge("RGB", (red_croped_image,
+                              green_croped_image,
+                              blue_croped_image))
     return img
 
-if __name__ == "__main__":
 
+def main():
     args = parse_arguments()
+
     file_path = args.path
+    origin_file_path = Path(file_path)
+    dir_abs_name = origin_file_path.parent
+    file_name = origin_file_path.name
 
-    abs_path = os.path.abspath(file_path)
-    dir_abs_name = os.path.dirname(abs_path)
-    file_name = os.path.basename(abs_path)
+    image = Image.open(origin_file_path)
+    image = image.convert("RGB")
 
-    image = Image.open(abs_path)
-    image_rgb = image.convert("RGB")
-    new_image_full = shift_image_layer(image_rgb, 10)
-    new_image_full.save(dir_abs_name + "/" + file_name + "_edited.jpg", "JPEG")
+    precision = args.precision
+    if precision >= 100:
+        precision = 100
+    elif precision <= 0:
+        precision = 0
+    shift_power = int(image.width / 100 * precision)
 
-    new_image_full.thumbnail((args.resolution, args.resolution))
-    new_image_full.save(dir_abs_name + "/" + file_name + f"_edited_thumbnail_{args.resolution}.jpg", "JPEG")
+    image = shift_image_layers(image, shift_power)
+    image.save(str(Path(dir_abs_name, file_name + "_edited.jpg")), "JPEG")
+    image.thumbnail((args.resolution, args.resolution))
+    image.save(str(Path(dir_abs_name,
+               file_name +
+               f"_edited_thumbnail_{args.resolution}.jpg")),
+               "JPEG")
+
+
+if __name__ == "__main__":
+    main()
